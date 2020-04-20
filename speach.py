0d421292b5e4215cf77c26cc4dc84e6d6ffbc23a
moysn7 <39126277+moysn7@users.noreply.github.com>
2020-04-20 19:39:19 +0200

    Add files via upload

--- speach.py
+++ speach.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from os import environ, path
 import pyaudio
+import os
 
 from pocketsphinx.pocketsphinx import *
 from sphinxbase.sphinxbase import *
@@ -22,9 +23,13 @@ decoder = Decoder(config)
 #diccionario inglés
 config = Decoder.default_config()
 config.set_string('-hmm', path.join(model_path + '/en-us'))
-config.set_string('-lm', path.join(model_path + '/en-us.lm.bin'))
-config.set_string('-dict', path.join(model_path + '/cmudict-en-us.dict'))
+#config.set_string('-lm', path.join(model_path + '/en-us.lm.bin'))
+config.set_string('-lm','./options/0534.lm')
+#config.set_string('-dict', path.join(model_path + '/cmudict-en-us.dict'))
+config.set_string('-dict','./options/0534.dic')
+config.set_string('-logfn', '/dev/null')
 decoder = Decoder(config)
+
 
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
@@ -32,6 +37,10 @@ stream.start_stream()
 
 in_speech_bf = False
 decoder.start_utt()
+
+print('---WELCOME TO SPEACH RECOGNITION---')
+print('To see current optios say SHOW OPTIONS')
+print('Say Exit to quit the aplication')
 
 while True:
     buf = stream.read(1024)
@@ -41,12 +50,27 @@ while True:
             in_speech_bf = decoder.get_in_speech()
             if not in_speech_bf:
                 decoder.end_utt()
-                print 'Result:', decoder.hyp().hypstr
-                if decoder.hyp().hypstr == 'uno':
-                    print 'Hasta luego Lucas'
-                    break
-                decoder.start_utt()
+               # print ('Result:', decoder.hyp().hypstr)
+                if decoder.hyp().hypstr == 'SHOW OPTIONS':
+                    os.system('cat corpus.txt')
+                    print('\n')
+                if decoder.hyp().hypstr == 'OPEN BROWSER':
+                    print('opening browser\n')
+                    os.system('open -a Safari.app')
+                if decoder.hyp().hypstr == 'NEW E-MAIL':
+                    print('opening MAIL client\n')
+                    os.system('open -a Mail.app')
+                if decoder.hyp().hypstr == 'OPEN MUSIC PLAYER':
+                    print('opening music player\n')
+                    os.system('open -a Spotify.app')
+                if decoder.hyp().hypstr == 'OPEN CALENDAR':
+                    print('opening calendar\n')
+                    os.system('open -a Calendar.app')
+                if decoder.hyp().hypstr == 'EXIT':
+                    print('BYE')
+                    quit()
 
+                decoder.start_utt()
     else:
         break
 decoder.end_utt()
